7bf8336db06370bed2c8b578afadc1a763075d73
rcmtcristian <rcmtcristian@gmail.com>
2022-10-09 18:48:56 -0400

    feat: started doc

--- Ring/Round 4/synchronous/js/main.js
+++ Ring/Round 4/synchronous/js/main.js
@@ -1,1 +1,10 @@
+let test = document.querySelector(".test");
 
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve((test.innerHTML = "done")), 1000);
+});
+
+promise.then(
+  (result) => alert(result),
+  (error) => alert(error)
+);
